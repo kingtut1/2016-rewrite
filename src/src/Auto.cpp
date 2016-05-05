9822ce3f7fc5685a6df12705a488476e460f8a5c
Robotics <Robotics@Robotics-PC.browardschools.local>
2016-05-05 13:44:54 -0400

    Thursday May 5th

---
+++ src/src/Auto.cpp
@@ -0,0 +1,18 @@
+/*
+ * Auto.cpp
+ *
+ *  Created on: May 2, 2016
+ *      Author: Robotics
+ */
+
+#include <Auto.h>
+
+Auto::Auto() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Auto::~Auto() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/src/SigmaDrive.h
@@ -0,0 +1,29 @@
+/*
+ * SigmaDrive.h
+ *
+ *  Created on: May 2, 2016
+ *      Author: Robotics
+ */
+#include "WPILib.h"
+#ifndef SRC_SIGMADRIVE_H_
+#define SRC_SIGMADRIVE_H_
+
+class SigmaDrive
+{
+public:
+	VictorSP *left1,*left2,*right1,*right2;
+	RobotDrive *drive108;
+	DoubleSolenoid *shifter, *shooterAim;
+	Encoder *leftEnc, *rightEnc;
+	ADXRS450_Gyro *gyro;
+	DigitalInput *Upperlimit, *Lowerlimit;
+	SigmaDrive();
+	void TankDrive(double l, double r);
+	void TankDrive(Joystick l, Joystick r);
+	void Shift(bool High);
+	virtual ~SigmaDrive();
+	void Shooter(bool Up);
+	void Arm(bool Up);
+
+};
+#endif /* SRC_SIGMADRIVE_H_ */
