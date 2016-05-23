6b27b207e4b017e4828859aacf2f1455da00b0fc
Robotics <Robotics@Robotics-PC.browardschools.local>
2016-05-23 16:41:17 -0400

    Starting vision

---
+++ src/src/Vision.cpp
@@ -0,0 +1,21 @@
+/*
+ * Vision.cpp
+ *
+ *  Created on: May 2, 2016
+ *      Author: Robotics
+ */
+
+#include <Vision.h>
+
+Vision::Vision() {
+	// TODO Auto-generated constructor stub
+
+}
+Vision::~Vision() {
+	// TODO Auto-generated destructor stub
+}
+void Vision::GetDistance()
+{
+	//Code for distance here
+}
+
---
+++ src/src/Vision.h
@@ -0,0 +1,18 @@
+/*
+ *Vision.h
+ *
+ *  Created on: May 2, 2016
+ *      Author: Robotics
+ */
+
+#ifndef SRC_VISION_H_
+#define SRC_VISION_H_
+
+class Vision {
+public:
+	Vision();
+	void GetDistance();
+	virtual ~Vision();
+};
+
+#endif /* SRC_VISION_H_ */
